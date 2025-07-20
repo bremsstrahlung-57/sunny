a43f383173409f5dcdb8cf65eeebb1552a74d66d
Sagar <sagar292905@gmail.com>
2025-07-20 15:27:48 +0530

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 echo "🌤️ Installing Sunny Weather CLI..."
-curl -L -o sunny.whl "https://github.com/bremsstrahlung-57/sunny/releases/download/v1.0.0/sunny-1.0.0-py3-none-any.whl"
-python3 -m pip install sunny.whl
-rm sunny.whl
-echo "✅ Installation complete! Try running: sunny"
+curl -L -O https://github.com/bremsstrahlung-57/sunny/releases/download/v1.0.2/sunny-1.0.2-py3-none-any.whl
+python3 -m pip install sunny-1.0.2-py3-none-any.whl
+rm sunny-1.0.2-py3-none-any.whl
+echo "✅ Done! Run it with: sunny"
