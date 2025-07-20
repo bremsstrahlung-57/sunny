669aed269e917fd101e7bf4c5a8f965586833700
Sagar <sagar292905@gmail.com>
2025-07-20 13:14:08 +0530

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="sunny",
     version="1.0.0",
-    description="A minimal CLI weather tool",
+    description="A minimal CLI tool to see weather on your terminal",
     author="Sagar Sharma",
-    author_email="sagarsharma.ai@protonmail.com",
+    author_email="sagar292905@gmail.com",
     url="https://github.com/bremsstrahlung-57/sunny",
     packages=find_packages(),
     include_package_data=True,
-    entry_points={"console_scripts": ["sunny=sunny.main:main"]},
+    entry_points={"console_scripts": ["sunny=sunny.__main__:main"]},
     install_requires=["requests", "rich", "toml"],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,5 +18,5 @@ setup(
         "Environment :: Console",
         "Topic :: Utilities",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
